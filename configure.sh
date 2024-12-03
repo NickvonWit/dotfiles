f2364684aa172eebdfc027ea95508cc1a4088f51
nschaller <nschaller@student.ethz.ch>
2024-12-03 21:54:25 +0100

    fixed fzf install logic

--- configure.sh
+++ configure.sh
@@ -150,13 +150,20 @@ else
 fi
 
 # Check if fzf is installed
-if [ -z "$(command -v fzf)" ]; then
-  echo "Installing fzf..."
-  if [ "$OS" == "mac" ]; then
+if [ "$OS" == "mac" ]; then
+  if [ -z "$(command -v fzf)" ]; then
+    echo "Installing fzf..."
     brew install fzf
-  elif [ "$OS" == "linux" ]; then
+  else
+    echo "fzf is already installed."
+  fi
+elif [ "$OS" == "linux" ]; then
+  if [ ! -d "$HOME/.fzf" ]; then
+    echo "Installing fzf..."
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
+  else
+    echo "fzf is already installed."
   fi
 fi
 #_____________________ Git Configuration _____________________
