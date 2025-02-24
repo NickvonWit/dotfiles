b7ec1c3bad882ec8bd8a3429658fa31c30fce790
nschaller <nschaller@student.ethz.ch>
2025-02-24 15:30:34 +0100

    no clue what changed:)

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -10,14 +10,14 @@ return {
 
     -- Set header
     dashboard.section.header.val = {
-        "                                                     ",
-        "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-        "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-        "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-        "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-        "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-        "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-        "                                                     ",
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
     }
 
     -- Set header highlight group
@@ -25,16 +25,16 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-        dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-        dashboard.button( "f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
-        dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-        dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-        dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+      dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "  > Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+      dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
     }
 
-    local fortune = require("alpha.fortune") 
+    local fortune = require("alpha.fortune")
     dashboard.section.footer.val = fortune()
-    
+
     -- Set footer highlight group
     dashboard.section.footer.opts.hl = "DashboardFooter"
 
@@ -48,21 +48,27 @@ return {
 
     -- Set random colors for header and footer
     vim.api.nvim_create_autocmd("VimEnter", {
-        once = true,
-        callback = function()
-            math.randomseed(os.time())
-            local colors = {
-                "#e06c75", "#98c379", "#e5c07b", "#61afef", 
-                "#c678dd", "#56b6c2", "#a9b1d6", "#f44747"
-            }
-            -- Random color for header
-            local header_color = colors[math.random(#colors)]
-            -- Different random color for footer
-            local footer_color = header_color
-            
-            vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = header_color })
-            vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = footer_color })
-        end
+      once = true,
+      callback = function()
+        math.randomseed(os.time())
+        local colors = {
+          "#e06c75",
+          "#98c379",
+          "#e5c07b",
+          "#61afef",
+          "#c678dd",
+          "#56b6c2",
+          "#a9b1d6",
+          "#f44747",
+        }
+        -- Random color for header
+        local header_color = colors[math.random(#colors)]
+        -- Different random color for footer
+        local footer_color = header_color
+
+        vim.api.nvim_set_hl(0, "DashboardHeader", { fg = header_color })
+        vim.api.nvim_set_hl(0, "DashboardFooter", { fg = footer_color })
+      end,
     })
   end,
 }
