d592c536909ed2a8fe9afa03011ff92247dffcac
nschaller <nschaller@student.ethz.ch>
2026-03-25 17:26:29 +0100

    remove CMakeLists.txt as root marker

--- nvim/.config/nvim/after/lsp/clangd.lua
+++ nvim/.config/nvim/after/lsp/clangd.lua
@@ -1,26 +1,25 @@
 return {
-  cmd = {
-    'clangd',
-    '--clang-tidy',
-    '--background-index',
-    '--offset-encoding=utf-8',
-    '--fallback-style=Google',
-  },
-
-  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
-  offsetEncoding = { 'utf-8', 'utf-16' },
-
-  root_markers = {
-    'CMakeLists.txt',
-    'build/',
-    'Makefile'
-  },
-
-  capabilities = {
-    textDocument = {
-      completion = {
-        editsNearCursor = true,
-      },
+    cmd = {
+        'clangd',
+        '--clang-tidy',
+        '--background-index',
+        '--offset-encoding=utf-8',
+        '--fallback-style=Google',
     },
-  },
+
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+    offsetEncoding = { 'utf-8', 'utf-16' },
+
+    root_markers = {
+        'build/',
+        'Makefile'
+    },
+
+    capabilities = {
+        textDocument = {
+            completion = {
+                editsNearCursor = true,
+            },
+        },
+    },
 }
